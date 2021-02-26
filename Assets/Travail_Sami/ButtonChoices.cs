baff8efa9922ac529bb602d65165073de35eab49
DESKTOP-8SACAPC\samit <sami.torbey@e-artsup.net>
2021-02-26 20:03:42 +0100

    Les bouttons Heaven/Hell font changer l'image

--- Assets/Travail_Sami/ButtonChoices.cs
+++ Assets/Travail_Sami/ButtonChoices.cs
@@ -1,19 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ButtonChoices : MonoBehaviour
 {
 
+    public Texture Happy;
+    public Texture Sad;
+    public Texture Waiting;
 
+    RawImage m_RawImage;
 
-    void HeavenChoice()
+    void Start()
     {
-
+        m_RawImage = GetComponent<RawImage>();
+        m_RawImage.texture = Waiting;
     }
 
-    void HellChoice()
+    public void HeavenChoice()
     {
+        m_RawImage.texture = Happy;
+    }
 
+    public void HellChoice()
+    {
+        m_RawImage.texture = Sad;
     }
 }
