4a0065ad05d6968f0ed20db5815de003491ee356
Drasibaoh <71836998+Drasibaoh@users.noreply.github.com>
2021-02-26 23:57:20 +0100

    Changement d'un canvas a l'autre implémenter

---
+++ Assets/Travail_Noah/GameManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public List<GameObject> canvas;
+    private GameObject actualCanvas;
+    public static GameManager _instance;
+    public bool firsTime=true;
+    public bool end = false;
+    private void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void NextJudgement()
+    {
+        if (canvas.Count-1>0 && firsTime)
+        {
+            int rand = Random.Range(0, canvas.Count - 1);
+            canvas[rand].SetActive(true);
+            actualCanvas = canvas[rand];
+            canvas.RemoveAt(rand);
+            firsTime = false;
+        }
+        else if (canvas.Count-1 > 0 && !firsTime)
+        {
+            actualCanvas.SetActive(false);
+            int rand = Random.Range(0, canvas.Count-1);
+            canvas[rand].SetActive(true);
+            actualCanvas= canvas[rand];
+            canvas.RemoveAt(rand);
+            
+        }
+        else 
+        {
+            end = true;
+            Debug.Log(end);
+        }
+
+
+    }
+
+    
+}
