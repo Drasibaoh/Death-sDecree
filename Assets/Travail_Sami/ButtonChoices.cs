edae78f7f2845dbbbded3094447a8f6b81d280e0
DESKTOP-8SACAPC\samit <sami.torbey@e-artsup.net>
2021-02-26 19:33:24 +0100

    Crée mon script de ButtonChoice, crée 3 bouttons dans mon scène

---
+++ Assets/Travail_Sami/ButtonChoices.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonChoices : MonoBehaviour
+{
+
+
+
+    void HeavenChoice()
+    {
+
+    }
+
+    void HellChoice()
+    {
+
+    }
+}
