68847ac5619608c60625f240d88d98e437794674
Drasibaoh <71836998+Drasibaoh@users.noreply.github.com>
2021-02-27 15:02:22 +0100

    Ending Codé

---
+++ Assets/Travail_Noah/EndingDialogue.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class EndingDialogue : MonoBehaviour
+{
+    public List<string> dialogues;
+    public List<bool> isPlayer;
+    public Text player;
+    public Text other;
+    public int current;
+    public GameObject outro;
+    void Update()
+    {
+
+    }
+    void Start()
+    {
+        player = this.GetComponent<Text>();
+        other.text = dialogues[current];
+    }
+
+    public void NextLine()
+    {
+        if (GameManager._instance.end)
+        {
+            current++;
+            if (current > dialogues.Count - 1)
+            {
+                outro.SetActive(false);
+                SceneManager.LoadScene(1);
+            }
+            else
+            {
+                if (isPlayer[current])
+                {
+                    player.text = dialogues[current];
+                }
+                else
+                {
+                    other.text = dialogues[current];
+                }
+            }
+        }
+    }
+}
--- Assets/Travail_Noah/GameManager.cs
+++ Assets/Travail_Noah/GameManager.cs
@@ -31,10 +31,7 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (end)
-        {
-            SceneManager.LoadScene(1);
-        }
+
     }
     public void NextJudgementHeaven()
     {
