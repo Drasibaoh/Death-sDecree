c24fbdb2ee40044deb3b8f5a3e7d39a038422b80
Drasibaoh <71836998+Drasibaoh@users.noreply.github.com>
2021-02-28 14:01:37 +0100

    Fade In/Out ajouté

---
+++ Assets/Travail_Noah/Scripts/LevelChanger.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class LevelChanger : MonoBehaviour
+{
+    public static LevelChanger _instance;
+    public Animator animator;
+    private int levelToLoad;
+    private void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    public void FadeToLevel(int levelIndex)
+    {
+        levelToLoad = levelIndex;
+        animator.SetTrigger("FadeOut");
+    }
+    public void OnFadeCompleted()
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
+}
